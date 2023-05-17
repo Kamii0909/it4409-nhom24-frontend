27efca6fdb0ce1a66219fe2b02896d4c461b331c
TungLam1410 <lam2002ttb@gmail.com>
2023-05-17 10:03:36 +0700

    Update Hotel Detail Page (structure + CSS + sample data)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+    return (
+        <div className="page-footer">
+            <p>*Một số khách sạn yêu cầu hủy hơn 24 giờ trước ngày nhận phòng. Xem thêm chi tiết trên trang.</p>
+            <p>© 2023 Hotels.com là công ty thuộc Expedia Group. Mọi quyền được bảo lưu.</p>
+            <p>Hotels.com và logo Hotels.com là thương hiệu hoặc thương hiệu đã đăng ký bảo hộ của Hotels.com, LP tại Mỹ và/hoặc các quốc gia khác. Mọi thương hiệu khác thuộc quyền sở hữu của các chủ sở hữu tương ứng.</p>
+        </div>
+    );
+};
+
+export default Footer;
