3f934222fd294d406d2f4067755f70b4bbbc10d5
ktcshust <kt209stark@gmail.com>
2023-06-11 23:01:37 +0700

    abcxyz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// Main.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import CRUDTab from "./CRUDTab";
+import SearchTab from "./SearchTab";
+import ThreeStars from "./ThreeStars";
+import FourStars from "./FourStars";
+import FiveStars from "./FiveStars";
+import Hanoi from "./Hanoi";
+import Halong from "./Halong";
+import Danang from "./Danang";
+import NhaTrang from "./NhaTrang";
+import HoChiMinh from "./HoChiMinh";
+import UserDetail from "./UserDetail"
+// Import các component tương ứng với các file Age khác
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/crud" element={<CRUDTab />} />
+          <Route path="/" element={<SearchTab />} />
+          <Route path="/users/:userId" element={<UserDetail />} />
+          <Route path="/threestars" element={<ThreeStars />} />
+          <Route path="/fourstars" element={<FourStars />} />
+          <Route path="/fivestars" element={<FiveStars />} />
+          <Route path="/hanoi" element={<Hanoi />} />
+          <Route path="/halong" element={<Halong />} />
+          <Route path="/danang" element={<Danang />} />
+          <Route path="/nhatrang" element={<NhaTrang />} />
+          <Route path="/hochiminh" element={<HoChiMinh />} />
+          {/* Thêm các Route tương ứng với các file Age khác */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
